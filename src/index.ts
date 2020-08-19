b0bd241c53fdc7606b737bd6d7d2adaf4f6575d7
Thaisah van Vugt <thaisah.van.vugt@orangenxt.com>
2020-08-19 13:38:55 +0200

    fix ionic+ typeLabel

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { IonicModule } from 'ionic-angular';
+import { IonicModule } from '@ionic/angular';
 import { IonCurrencyMaskComponent } from './ion-currency-mask/ion-currency-mask.component';
 
 
